e1a5e3fa095d4eb5fed8e3eca21c1b38b6034fcf
Sergey <sergey15.aliev@gmail.com>
2022-02-07 22:39:19 +0200

    update

--- template-php/phppart/head.php
+++
@@ -1,9 +0,0 @@
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width; initial-scale=1">
-	<link rel="stylesheet" href="css/all.css" type="text/css"/>
-	<link rel="stylesheet" href="sass/fonts.css" type="text/css"/>
-	<script type="text/javascript" src="js/jquery-3.2.0.js"></script>
-	<script type="text/javascript" src="js/main.js"></script>
-	<title>TITLE</title>
-</head>
--- template-scss/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-});
-
-// Burger Menu
-$(document).ready(function() {
-    $('.header__menu-burger').click(function(event) {
-        $('.burger-menu').toggleClass('active');
-    });
-});
