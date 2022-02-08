c4336461ab294d0c6e31c9201e057b4b6f6574d5
Sergey Drevoline <sergey.drevoline@gmail.com>
2022-02-08 15:03:11 +0200

    update

--- template-scss/assets/js/files/script.js
+++ template-scss/assets/js/files/script.js
@@ -1,6 +1,40 @@
+//=================
+//Menu
 $(function () {
     $('.burger-menu').click(function (event) {
         $('.menu').toggleClass('active')
-    })
+    }),
+    $('.submenu__link, .menu__link').click(function (event) {
+        $('.menu').removeClass('active')
+    });
 })
+//=================
+//Anchor
+$(document).ready(function(){
+	$("#menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
+//=================
+//Scroll to top
+var btn = $('#scrollToTop');
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
 
