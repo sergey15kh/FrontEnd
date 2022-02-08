be73bbd09282156481dc85a60b706bfdd5802664
Sergey Aliev <62428651+sergey15kh@users.noreply.github.com>
2022-02-08 22:54:15 +0200

    update

--- template-scss/assets/js/files/script.js
+++ template-scss/assets/js/files/script.js
@@ -38,3 +38,16 @@ btn.on('click', function(e) {
 });
 
 
+$(function () {
+  $(".col-md-4").slice(0, 3).show();
+  $("body").on('click touchstart', '.load-more', function (e) {
+    e.preventDefault();
+    $(".col-md-4:hidden").slice(0, 3).slideDown();
+    if ($(".col-md-4:hidden").length == 0) {
+      $(".load-more").css('visibility', 'hidden');
+    }
+    $('html,body').animate({
+      scrollTop: $(this).offset().top
+    }, 1000);
+  });
+});
