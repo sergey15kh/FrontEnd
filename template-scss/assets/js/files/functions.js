e00a0aab688bdc7da378d40afaed0ff04384397c
Sergey <sergey15.aliev@gmail.com>
2022-02-12 13:57:13 +0200

    update

--- template-scss/assets/js/files/functions.js
+++ template-scss/assets/js/files/functions.js
@@ -177,39 +177,39 @@ for (let index = 0; index < tabs.length; index++) {
 }
 //=================
 /*
-Для родителя слойлеров пишем атрибут data-spollers
-Для заголовков слойлеров пишем атрибут data-spoller
+Для родителя слойлеров пишем атрибут data-spoilers
+Для заголовков слойлеров пишем атрибут data-spoiler
 Если нужно включать\выключать работу спойлеров на разных размерах экранов
 пишем параметры ширины и типа брейкпоинта.
 Например: 
-data-spollers="992,max" - спойлеры будут работать только на экранах меньше или равно 992px
-data-spollers="768,min" - спойлеры будут работать только на экранах больше или равно 768px
+data-spoilers="992,max" - спойлеры будут работать только на экранах меньше или равно 992px
+data-spoilers="768,min" - спойлеры будут работать только на экранах больше или равно 768px
 
-Если нужно что бы в блоке открывался болько один слойлер добавляем атрибут data-one-spoller
+Если нужно что бы в блоке открывался болько один слойлер добавляем атрибут data-one-spoiler
 */
 
-// SPOLLERS
-const spollersArray = document.querySelectorAll('[data-spollers]');
-if (spollersArray.length > 0) {
+// spoilerS
+const spoilersArray = document.querySelectorAll('[data-spoilers]');
+if (spoilersArray.length > 0) {
 	// Получение обычных слойлеров
-	const spollersRegular = Array.from(spollersArray).filter(function (item, index, self) {
-		return !item.dataset.spollers.split(",")[0];
+	const spoilersRegular = Array.from(spoilersArray).filter(function (item, index, self) {
+		return !item.dataset.spoilers.split(",")[0];
 	});
 	// Инициализация обычных слойлеров
-	if (spollersRegular.length > 0) {
-		initSpollers(spollersRegular);
+	if (spoilersRegular.length > 0) {
+		initspoilers(spoilersRegular);
 	}
 
 	// Получение слойлеров с медиа запросами
-	const spollersMedia = Array.from(spollersArray).filter(function (item, index, self) {
-		return item.dataset.spollers.split(",")[0];
+	const spoilersMedia = Array.from(spoilersArray).filter(function (item, index, self) {
+		return item.dataset.spoilers.split(",")[0];
 	});
 
 	// Инициализация слойлеров с медиа запросами
-	if (spollersMedia.length > 0) {
+	if (spoilersMedia.length > 0) {
 		const breakpointsArray = [];
-		spollersMedia.forEach(item => {
-			const params = item.dataset.spollers;
+		spoilersMedia.forEach(item => {
+			const params = item.dataset.spoilers;
 			const breakpoint = {};
 			const paramsArray = params.split(",");
 			breakpoint.value = paramsArray[0];
@@ -234,71 +234,71 @@ if (spollersArray.length > 0) {
 			const matchMedia = window.matchMedia(paramsArray[0]);
 
 			// Объекты с нужными условиями
-			const spollersArray = breakpointsArray.filter(function (item) {
+			const spoilersArray = breakpointsArray.filter(function (item) {
 				if (item.value === mediaBreakpoint && item.type === mediaType) {
 					return true;
 				}
 			});
 			// Событие
 			matchMedia.addListener(function () {
-				initSpollers(spollersArray, matchMedia);
+				initspoilers(spoilersArray, matchMedia);
 			});
-			initSpollers(spollersArray, matchMedia);
+			initspoilers(spoilersArray, matchMedia);
 		});
 	}
 	// Инициализация
-	function initSpollers(spollersArray, matchMedia = false) {
-		spollersArray.forEach(spollersBlock => {
-			spollersBlock = matchMedia ? spollersBlock.item : spollersBlock;
+	function initspoilers(spoilersArray, matchMedia = false) {
+		spoilersArray.forEach(spoilersBlock => {
+			spoilersBlock = matchMedia ? spoilersBlock.item : spoilersBlock;
 			if (matchMedia.matches || !matchMedia) {
-				spollersBlock.classList.add('_init');
-				initSpollerBody(spollersBlock);
-				spollersBlock.addEventListener("click", setSpollerAction);
+				spoilersBlock.classList.add('_init');
+				initspoilerBody(spoilersBlock);
+				spoilersBlock.addEventListener("click", setspoilerAction);
 			} else {
-				spollersBlock.classList.remove('_init');
-				initSpollerBody(spollersBlock, false);
-				spollersBlock.removeEventListener("click", setSpollerAction);
+				spoilersBlock.classList.remove('_init');
+				initspoilerBody(spoilersBlock, false);
+				spoilersBlock.removeEventListener("click", setspoilerAction);
 			}
 		});
 	}
 	// Работа с контентом
-	function initSpollerBody(spollersBlock, hideSpollerBody = true) {
-		const spollerTitles = spollersBlock.querySelectorAll('[data-spoller]');
-		if (spollerTitles.length > 0) {
-			spollerTitles.forEach(spollerTitle => {
-				if (hideSpollerBody) {
-					spollerTitle.removeAttribute('tabindex');
-					if (!spollerTitle.classList.contains('_active')) {
-						spollerTitle.nextElementSibling.hidden = true;
+	function initspoilerBody(spoilersBlock, hidespoilerBody = true) {
+		const spoilerTitles = spoilersBlock.querySelectorAll('[data-spoiler]');
+		if (spoilerTitles.length > 0) {
+			spoilerTitles.forEach(spoilerTitle => {
+				if (hidespoilerBody) {
+					spoilerTitle.removeAttribute('tabindex');
+					if (!spoilerTitle.classList.contains('_active')) {
+						spoilerTitle.nextElementSibling.hidden = true;
 					}
 				} else {
-					spollerTitle.setAttribute('tabindex', '-1');
-					spollerTitle.nextElementSibling.hidden = false;
+					spoilerTitle.setAttribute('tabindex', '-1');
+					spoilerTitle.nextElementSibling.hidden = false;
 				}
 			});
 		}
 	}
-	function setSpollerAction(e) {
+	function setspoilerAction(e) {
 		const el = e.target;
-		if (el.hasAttribute('data-spoller') || el.closest('[data-spoller]')) {
-			const spollerTitle = el.hasAttribute('data-spoller') ? el : el.closest('[data-spoller]');
-			const spollersBlock = spollerTitle.closest('[data-spollers]');
-			const oneSpoller = spollersBlock.hasAttribute('data-one-spoller') ? true : false;
-			if (!spollersBlock.querySelectorAll('._slide').length) {
-				if (oneSpoller && !spollerTitle.classList.contains('_active')) {
-					hideSpollersBody(spollersBlock);
+		if (el.hasAttribute('data-spoiler') || el.closest('[data-spoiler]')) {
+			const spoilerTitle = el.hasAttribute('data-spoiler') ? el : el.closest('[data-spoiler]');
+			const spoilersBlock = spoilerTitle.closest('[data-spoilers]');
+			const onespoiler = spoilersBlock.hasAttribute('data-one-spoiler') ? true : false;
+			if (!spoilersBlock.querySelectorAll('._slide').length) {
+				if (onespoiler && !spoilerTitle.classList.contains('_active')) {
+					hidespoilersBody(spoilersBlock);
 				}
-				spollerTitle.classList.toggle('_active');
-				_slideToggle(spollerTitle.nextElementSibling, 500);
+				spoilerTitle.classList.toggle('_active');
+				_slideToggle(spoilerTitle.nextElementSibling, 500);
 			}
 			e.preventDefault();
 		}
 	}
-	function hideSpollersBody(spollersBlock) {
-		const spollerActiveTitle = spollersBlock.querySelector('[data-spoller]._active');
-		if (spollerActiveTitle) {
-			spollerActiveTitle.classList.remove('_active');
-			_slideUp(spollerActiveTitle.nextElementSibling, 500);
+	function hidespoilersBody(spoilersBlock) {
+		const spoilerActiveTitle = spoilersBlock.querySelector('[data-spoiler]._active');
+		if (spoilerActiveTitle) {
+			spoilerActiveTitle.classList.remove('_active');
+			_slideUp(spoilerActiveTitle.nextElementSibling, 500);
 		}
 	}
 }
